d46833fc6eabf8f8ff5fcdecc2ce2f27f732622f
laboratorio <laboratorio@UPOAULA10702>
2025-05-22 07:50:46 -0500

    Agregar archivos de proyecto.

---
+++ AntambaJTallerLayuot/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AntambaJTallerLayuot
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AntambaJTallerLayuot/Demo.xaml.cs
@@ -0,0 +1,9 @@
+namespace AntambaJTallerLayuot;
+
+public partial class Demo : ContentPage
+{
+	public Demo()
+	{
+		InitializeComponent();
+	}
+}
